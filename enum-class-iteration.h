3e9cefc625258369d7faa9bc434bf032d206e482
Frederic Perez <frederic.perez@about.me>
2017-10-07 09:01:18 +0000

    master COMP: Simplify definition of straightforward enum classes

--- enum-class-iteration.h
+++ enum-class-iteration.h
@@ -7,22 +7,11 @@ namespace enums {
 template <class T>
 constexpr
 size_t
-min() {
-	return static_cast<size_t>(T::min);
-}
-
-template <class T>
-constexpr
-size_t
-max() {
-	return static_cast<size_t>(T::max);
-}
-
-template <class T>
-constexpr
-size_t
-count_defined() { // Note: This works for consecutive enum values; if not > Specialize!
-	return max<T>() - min<T>() + 1;
+count_defined() { 
+  // Note: This works for 
+  // 1. consecutive enum values, starting with "zero"; if not > Specialize, and
+  // 2. `undefined` must be the last value of the enum class
+	return static_cast<size_t>(T::undefined);
 }
 
 enum class Planet : uint8_t {
@@ -34,16 +23,12 @@ enum class Planet : uint8_t {
   Saturn,
   Uranus,
   Neptune,
-  min = Mercury,
-  max = Neptune,
   undefined
 };
 
 enum class Opposite : uint8_t {
   yin,
   yang,
-  min = yin,
-  max = yang,
   undefined
 };
 
@@ -58,13 +43,14 @@ constexpr std::array<Planet, count_defined<Planet>()>
     Planet::Uranus,
     Planet::Neptune
   }};
-static_assert(all_defined_planets.front() == Planet::min, "static_assert failed: Bad array--check min");
-static_assert(all_defined_planets.back() == Planet::max, "static_assert failed: Bad array--check max");
+static_assert(all_defined_planets.back() != static_cast<Planet>(0), "static_assert failed: Bad array--check contents to satisfy correct size");
 
 constexpr std::array<Opposite, count_defined<Opposite>()>
-  all_defined_opposites{{ Opposite::yin, Opposite::yang }};
-static_assert(all_defined_opposites.front() == Opposite::min, "static_assert failed: Bad array--check min");
-static_assert(all_defined_opposites.back() == Opposite::max, "static_assert failed: Bad array--check max");
+  all_defined_opposites{{
+    Opposite::yin,
+    Opposite::yang
+  }};
+static_assert(all_defined_opposites.back() != static_cast<Opposite>(0), "static_assert failed: Bad array--check contents to satisfy correct size");
 
 void ExamplesOfEnumClassIteration();
 
